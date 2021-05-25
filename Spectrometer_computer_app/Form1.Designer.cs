f53f3fe966729bf1efdf343f553c847311da7344
Slevay <honzik.sliva@seznam.cz>
2021-05-25 21:58:23 +0200

    Add project files.

---
+++ Spectrometer_computer_app/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace Spectrometer_computer_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelSpektrometr = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.FreezeButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelSpektrometr
+            // 
+            this.labelSpektrometr.AutoSize = true;
+            this.labelSpektrometr.Font = new System.Drawing.Font("Segoe UI", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelSpektrometr.Location = new System.Drawing.Point(3, 0);
+            this.labelSpektrometr.Name = "labelSpektrometr";
+            this.labelSpektrometr.Size = new System.Drawing.Size(281, 59);
+            this.labelSpektrometr.TabIndex = 1;
+            this.labelSpektrometr.Text = "Spectrometer";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 146);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(12, 401);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(158, 45);
+            this.SaveButton.TabIndex = 3;
+            this.SaveButton.Text = "Save current frame";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(103, 59);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(134, 23);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 21);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Serial port:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(361, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 43);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Start streaming";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(523, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 43);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "End streaming";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.EndButton_Click);
+            // 
+            // FreezeButton
+            // 
+            this.FreezeButton.Location = new System.Drawing.Point(361, 61);
+            this.FreezeButton.Name = "FreezeButton";
+            this.FreezeButton.Size = new System.Drawing.Size(320, 43);
+            this.FreezeButton.TabIndex = 8;
+            this.FreezeButton.Text = "Freeze";
+            this.FreezeButton.UseVisualStyleBackColor = true;
+            this.FreezeButton.Click += new System.EventHandler(this.FreezeButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Not connected";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(361, 146);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(12, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 25);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Current frame:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(361, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(168, 25);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Comparative frame:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(176, 401);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(156, 45);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Set as comparative frame";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.SetAsComparisonImage);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(361, 401);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(158, 45);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Save comparative frame";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.SaveComparisonImage);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 464);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.FreezeButton);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelSpektrometr);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_Closed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labelSpektrometr;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private bool IsFreezed = false;
+        private System.Windows.Forms.Button FreezeButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Spectrometer_computer_app/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+
+namespace Spectrometer_computer_app
+{
+    public partial class Form1 : Form
+    {
+        int Rows = 240;
+        int Columns = 320;
+        int BytesPerPixel = 2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            serialPort1 = new SerialPort();
+            serialPort1.BaudRate = 500000;
+            serialPort1.DataBits = 8;
+            serialPort1.StopBits = StopBits.One;
+            serialPort1.Handshake = Handshake.None;
+            serialPort1.Parity = Parity.None;
+            serialPort1.ReadBufferSize = 200000;
+
+            string[] ports = SerialPort.GetPortNames();
+            comboBox1.Items.AddRange(ports);
+            comboBox1.SelectedIndex = 0;
+
+            var _blankBitmap = new Bitmap(Columns, Rows, PixelFormat.Format16bppRgb565);
+            pictureBox1.Image = (Image)_blankBitmap.Clone();
+            pictureBox2.Image = (Image)_blankBitmap.Clone();
+        }
+
+        private void Form1_Closed(object sender, FormClosedEventArgs e)
+        {
+
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Write(new byte[] { 2 }, 0, 1);
+                StopStreaming();
+            }
+        }
+
+        private void StartButton_Click(object sender, System.EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                serialPort1.DataReceived += DataReceivedHandler;
+                serialPort1.PortName = comboBox1.Text;
+                try
+                {
+                    serialPort1.Open();
+                }
+                catch (IOException) { }
+            }
+
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.DiscardInBuffer();
+                serialPort1.Write(new byte[]{ 1 }, 0, 1);
+                label2.Text = "Connected to port " + comboBox1.Text;
+            }
+
+        }
+
+        private void EndButton_Click(object sender, System.EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Write(new byte[] { 2 }, 0, 1);
+                StopStreaming();
+            }
+            label2.Text = "Not connected";
+        }
+
+        private void StopStreaming()
+        {
+            serialPort1.DataReceived -= DataReceivedHandler;
+            Thread.Sleep(1000);
+            serialPort1.DiscardInBuffer();
+            Thread.Sleep(1000);
+            serialPort1.Close();
+        }
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e){
+            SerialPort port = (SerialPort)sender;
+
+            byte[] Buffer = new byte[Rows * Columns * BytesPerPixel];
+            Bitmap Bitmap = new Bitmap(Columns, Rows, PixelFormat.Format16bppRgb565);
+
+            int position = 0;
+            int plusPosition = 0;
+
+            bool show = true;
+
+            try
+            {
+                while (position < Rows * Columns * BytesPerPixel)
+                {
+                    plusPosition = port.BytesToRead;
+                    port.Read(Buffer, position, Math.Min(Rows * Columns * BytesPerPixel - position, plusPosition));
+                    position += plusPosition;
+                }
+            }
+            catch (InvalidOperationException)
+            {
+                show = false;
+            }
+            catch (TimeoutException)
+            {
+                show = false;
+            }
+
+            if (show)
+            {
+                for (int row = 0; row < Rows; row++)
+                {
+
+                    for (int column = 0; column < Columns; column++)
+                    {
+                        Bitmap.SetPixel(column, row, Color.FromArgb(
+                            (int)(Buffer[BytesPerPixel * Columns * row + BytesPerPixel * column] -
+                            (Buffer[BytesPerPixel * Columns * row + BytesPerPixel * column] % Math.Pow(2, 3))),
+                            (int)((Buffer[BytesPerPixel * Columns * row + BytesPerPixel * column] % Math.Pow(2, 3)) * Math.Pow(2, 5) +
+                            (Buffer[BytesPerPixel * Columns * row + BytesPerPixel * column + 1] -
+                            (Buffer[BytesPerPixel * Columns * row + BytesPerPixel * column + 1] % Math.Pow(2, 5))) / Math.Pow(2, 3)),
+                            (int)((Buffer[BytesPerPixel * Columns * row + BytesPerPixel * column + 1] % Math.Pow(2, 5)) * Math.Pow(2, 3))
+                            ));
+                    }
+                }
+                if (!IsFreezed) { pictureBox1.Image = (Image)Bitmap.Clone(); }
+            }
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                return;
+            }
+
+            var BitMapToSave = (Bitmap) pictureBox1.Image.Clone();
+
+            Stream myStream;
+
+            saveFileDialog1.Filter = "Png Image|*.png|JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif";
+            saveFileDialog1.FilterIndex = 0;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    BitMapToSave.Save(myStream, ImageFormat.Png);
+
+                    myStream.Close();
+                }
+            }
+        }
+
+        private void FreezeButton_Click(object sender, EventArgs e)
+        {
+            if (!IsFreezed)
+            {
+                FreezeButton.Text = "Freeze";
+            }
+            else
+            {
+                FreezeButton.Text = "Unfreeze";
+            }
+            IsFreezed = !IsFreezed;
+        }
+
+        private void SetAsComparisonImage(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                pictureBox2.Image = null;
+                return;
+            }
+            pictureBox2.Image = (Image)pictureBox1.Image.Clone();
+        }
+
+        private void SaveComparisonImage(object sender, EventArgs e)
+        {
+            if (pictureBox2.Image == null)
+            {
+                return;
+            }
+
+            var BitMapToSave = (Bitmap)pictureBox2.Image.Clone();
+
+            Stream myStream;
+
+            saveFileDialog1.Filter = "Png Image|*.png|JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif";
+            saveFileDialog1.FilterIndex = 0;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    BitMapToSave.Save(myStream, ImageFormat.Png);
+
+                    myStream.Close();
+                }
+            }
+        }
+    }
+}
